6eff215da1395defbe409654c35b5e296f2e185a
Kailey <k.kotynski@gmail.com>
2022-10-24 15:29:02 -0700

    Added axios and event listener for user login

---
+++ public/js/login.js
@@ -0,0 +1,25 @@
+// use axios to get post request
+const login = async (email, password) => {
+  console.log(email);
+  try {
+    const res = await axios({
+      method: "POST",
+      url: "http://localhost:3000/api/v1/users/login",
+      data: {
+        email,
+        password,
+      },
+    });
+    console.log(res);
+  } catch (err) {
+    console.log(err.response.data);
+  }
+};
+
+// event listener to listen for submit event
+document.querySelector(".form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  login(email, password);
+});
